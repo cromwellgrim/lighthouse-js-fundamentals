d7117a151024751f402cdc89da213303e74f2a76
cromwellgrim <rogers.dylan@protonmail.com>
2021-01-02 16:16:47 +0000

    from whichSchool challenge

---
+++ whichSchool.js
@@ -0,0 +1,9 @@
+const age = 45
+
+if (age < 13) {
+  return "Elementary School";
+} else if (age > 13 && age < 18) {
+  return "Secondary School";
+} else if (age > 18) {
+  return "Lighthouse Labs";
+}
