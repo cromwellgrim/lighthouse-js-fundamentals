ae5bbada2ce5bb964654e0a49d96b21c742b561b
cromwellgrim <rogers.dylan@protonmail.com>
2021-01-03 17:16:50 +0000

    more added to the hello.js file

--- hello.js
+++ hello.js
@@ -3,3 +3,14 @@ const sayHello = function (name) {
 }
 
 sayHello("Emily");
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('Kimia');
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("Veronica");
+console.log(greeting);
